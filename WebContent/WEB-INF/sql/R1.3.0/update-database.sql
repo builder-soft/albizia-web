7871f84e51fa519a2526606664b6fd80d420b46f
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-05-18 11:30:51 -0400

    Se complementa script para crear configuracion de modulos por dominio

--- WebContent/WEB-INF/sql/R1.3.0/update-database.sql
+++ WebContent/WEB-INF/sql/R1.3.0/update-database.sql
@@ -1,13 +1,38 @@
 delete from bsframework.tDomainAttribute;
+truncate table bsframework.tDomainAttribute;
 
+drop procedure if exists pUpdateData_Temp;
 DELIMITER $$
 create procedure pUpdateData_Temp()
 begin
 # Hay que inconrporar la creacion de los atributos TIMECTRL, ALBIZIA y DALEA 
 # en la tabla tConfig para cada dominio.
+	DECLARE vId BIGINT(20);
+	DECLARE vDone BOOLEAN DEFAULT FALSE;
+	DECLARE cursorDomain CURSOR FOR
+		SELECT	cId
+		FROM	bsframework.tDomain;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET vDone = TRUE;
 
-	bsframework.tDomainAttribute;
-	
+	OPEN cursorDomain;
+	cursorDomain_loop: LOOP
+		FETCH cursorDomain INTO vId;
+		
+		IF(vDone) THEN 
+			LEAVE cursorDomain_loop;
+		END IF;	
+
+#		select vId;
+		
+
+		INSERT INTO bsframework.tDomainAttribute(cDomain, cKey, cName, cValue) VALUES(vId, 'TIMECTRL', 'TIMECTRL', 'true');
+		INSERT INTO bsframework.tDomainAttribute(cDomain, cKey, cName, cValue) VALUES(vId, 'DALEA', 'DALEA', 'true');
+		INSERT INTO bsframework.tDomainAttribute(cDomain, cKey, cName, cValue) VALUES(vId, 'ALBIZIA', 'ALBIZIA', 'false');
+		
+	END LOOP cursorDomain_loop;
+	CLOSE cursorDomain;
+		
+#	bsframework.tDomainAttribute;
 #	IF(NOT EXISTS(SELECT cId FROM bsframework.tConfig WHERE cKey = 'DALEA_CONTEXT')) THEN
 #		INSERT INTO bsframework.tConfig(cKey, cValue) VALUES('DALEA_CONTEXT', '/dalea-web');
 #	END IF;
